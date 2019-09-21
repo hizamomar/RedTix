c883331b978f03d087db74223a25ede16cf3f704
Mohammad Norhizam Omar <hizam.omar@gmail.com>
2019-09-21 23:58:52 +0800

    Added Create User Page
    Basic validation for all field
    Email validation checking

---
+++ resources/views/redtixs/create.blade.php
@@ -0,0 +1,62 @@
+@extends('redtixs.layout')
+  
+@section('content')
+
+<div class="card uper">
+  <div class="card-header">
+    <h2>Add New User</h2>
+  </div>
+  <div class="card-body">  
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('redtixs.store') }}" method="POST">
+    @csrf
+  
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Email:</strong>
+                <input type="email" name="email" class="form-control" placeholder="Email" required>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>First Name:</strong>
+                <input type="text" name="first_name" class="form-control" placeholder="First Name" required>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Last Name:</strong>
+                <input type="text" name="last_name" class="form-control" placeholder="Last Name" required>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Status:</strong>
+                <select name="status" class="form-control" placeholder="Status" required>
+                  <option>Please Select</option>
+                  <option>Active</option>
+                  <option>Inactive</option>
+                </select>                
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+</div>
+</div>
+@endsection
